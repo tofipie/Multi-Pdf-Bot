6f0869e725f8b0aaf400243cbe07d5170521f17a
tofipie <62646269+tofipie@users.noreply.github.com>
2024-01-27 20:23:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -43,14 +43,15 @@ def get_conversation_chain(vectorstore):
      llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", 
                           model_kwargs={"temperature":0.5, "max_length":512})
 
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
-        conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
+     memory = ConversationBufferMemory(
+         
+         memory_key='chat_history', return_messages=True)
+         conversation_chain = ConversationalRetrievalChain.from_llm(
+         llm=llm,
+         retriever=vectorstore.as_retriever(),
+         memory=memory
     )
-    return conversation_chain
+     return conversation_chain
 
 
 
